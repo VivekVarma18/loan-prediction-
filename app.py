2508bf8af2a4392a09de10b8c10f8f05cd2cc480
VivekVarma18 <rudrarajuvevekvarma@gmail.com>
2025-03-26 16:24:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+st.title('Loan Approval process Automation')
+model=joblib.load('loan_data.joblib')
+gender=st.number_input("Enter gender male:0 female:1")
+married= st.number_input("enter marital status married:1 unmarried:0")
+income=st.number_input("enter applicant income in thousands")
+la=st.number_input("enter loan amount in thousands")
+if st.button("predict Approval"):
+    prediction=model.predict([[gender,married,income,la]])
+    if Prediction=='Y':
+        st.text("loan Approved")
+    else:
+        st.text("loan Rejected")
